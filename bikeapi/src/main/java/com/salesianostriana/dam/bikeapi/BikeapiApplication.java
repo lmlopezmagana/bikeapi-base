3af4e926afaf264446ee3aa8067d6d06a45b2a77
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-10-10 09:08:20 +0200

    proyecto generado

---
+++ bikeapi/src/main/java/com/salesianostriana/dam/bikeapi/BikeapiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.bikeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeapiApplication.class, args);
+	}
+
+}
